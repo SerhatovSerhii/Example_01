fc16102138e666f2479cc03ae97f3a87156ebbb2
Serhatov Serhii <su230484@gmail.com>
2023-11-17 11:26:06 +0100

    add y , x

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int y = 5;
+Orange orange = new Orange();
+int x = 10;
+        System.out.println(x);
 
     }
 }
---
+++ src/Orange.java
@@ -0,0 +1,5 @@
+public class Orange {
+    public static void main(String[] args) {
+        Orange orange = new Orange();
+    }
+}
